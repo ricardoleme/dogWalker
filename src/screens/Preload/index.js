65d12dd5af7c0cc71acc036e58c44ce4606f166d
Prof. Ms. Ricardo Leme <ricardo.leme@gmail.com>
2020-09-04 14:43:27 -0300

    arquivos iniciais do projeto

---
+++ src/screens/Preload/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Container } from './styles'
+import Barber from '../../components/icons/Barber'
+
+
+export default () => {
+    return (
+        <Container>
+            <Barber width="300"/>
+
+            
+        </Container>
+    )
+}
