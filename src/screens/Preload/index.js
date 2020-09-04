4b5d2c9913ab317c7e3df28189128995c4a33978
Ricardo Leme <ricardoleme@MacBook-Air-de-Ricardo.local>
2020-09-04 16:15:47 -0300

    new icon - Walker

--- src/screens/Preload/index.js
+++ src/screens/Preload/index.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import { Container } from './styles'
 import Barber from '../../components/icons/Barber'
+import Walker from '../../components/icons/Walker'
 
 
 export default () => {
     return (
         <Container>
-            <Barber width="300"/>
+            <Walker width="300"/>
 
             
         </Container>
